0f5412ae199d3aa6a394c7219d6fba93ba40c09d
Ezequiel Ferreyra <ezef8137@gmail.com>
2024-09-29 00:13:29 -0300

    Procedimientos en persona y usuario
    Se ajustaron SPB persona y SPB usuario

--- data/persona/SPB_persona.js
+++ data/persona/SPB_persona.js
@@ -12,7 +12,14 @@ const SPB_Persona = async (req, res) => {
       .input('IdPersona', IdPersona)
       .execute('SPB_Persona'); // Ejecuta el procedimiento almacenado en SQL Server
 
-    res.status(200).send(result.recordset);
+    const message = result.recordset[0].Message;
+
+    // Manejo de los códigos de estado basado en el mensaje
+    if (message.includes('éxito')) {
+      res.status(200).send({ message });
+    } else{
+      res.status(400).send({message})
+    }
   } catch (err) {
     console.error('Error al ejecutar el procedimiento almacenado:', err);
     res.status(500).send('Error en el servidor');
--- data/usuarios/SPB_usuarios.js
+++ data/usuarios/SPB_usuarios.js
@@ -1,33 +1,29 @@
 const { connect } = require("../../config");
 
 const SPB_Usuario = async (req, res) => {
-    console.log(req.body);
-    const { IdUsuario } = req.body;
-  
-    try {
-      const pool = await connect(); // Obtenemos la conexión de la función connect
-      const result = await pool.request()
-        .input('IdUsuario', sql.Int, IdUsuario) // Asegúrate de definir el tipo de dato
-        .execute('SPB_Usuario'); // Ejecuta el procedimiento almacenado
-  
-      // Verifica si hay resultados
-      if (result.recordset.length > 0) {
-        const message = result.recordset[0].Message; // Mensaje de retorno desde el procedimiento almacenado
-  
-        // Manejo de los códigos de estado basado en el mensaje
-        if (Message.includes('éxito')) {
-          res.status(200).send({ message });
-        } else {
-          res.status(400).send({ message });
-        }
-      } else {
-        res.status(404).send({ message: 'No se encontraron resultados.' });
-      }
-    } catch (err) {
-      console.error('Error al ejecutar el procedimiento almacenado:', err);
-      res.status(500).send('Error en el servidor');
+  console.log(req.body);
+  const {
+    IdUsuario
+  } = req.body;
+
+  try {
+    const pool = await connect(); // Obtenemos la conexión de la función connect
+    const result = await pool.request()
+      .input('IdUsuario', IdUsuario)
+      .execute('SPB_Usuario'); // Ejecuta el procedimiento almacenado en SQL Server
+
+    const message = result.recordset[0].Message;
+
+    // Manejo de los códigos de estado basado en el mensaje
+    if (message.includes('éxito')) {
+      res.status(200).send({ message });
+    } else{
+      res.status(400).send({message})
     }
-  };
-  
-  module.exports = { SPB_Usuario };
-  
+  } catch (err) {
+    console.error('Error al ejecutar el procedimiento almacenado:', err);
+    res.status(500).send('Error en el servidor');
+  }
+};
+
+module.exports = { SPB_Usuario };
